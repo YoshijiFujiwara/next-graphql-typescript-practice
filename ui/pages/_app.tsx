3e86f00e2ee5943dfedb82b141d7a5ecd6c3a16f
= <=>
2020-02-24 16:27:42 +0900

    add styling

---
+++ ui/pages/_app.tsx
@@ -0,0 +1,14 @@
+import "../styles/index.css";
+import React from "react";
+import App from "next/app";
+import Layout from "../components/Layout";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
